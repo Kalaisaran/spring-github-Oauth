1b0e9c8040bf93803f74857106f104adb94c7074
Kalaisaran <saravanan.kalaivanan@gmail.com>
2024-11-21 07:57:23 -0500

    Add files via upload

---
+++ src/main/java/io/kalaisaran/SpringGithubOAuthLoginStarterApplication.java
@@ -0,0 +1,13 @@
+package io.kalaisaran.spring_github_OAuth_login_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubOAuthLoginStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubOAuthLoginStarterApplication.class, args);
+	}
+
+}
