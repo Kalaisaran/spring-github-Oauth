d490a17e93024b1957c8929a933261a8376d0ed4
kalaisaran <saravanan.kalaivanan@gmail.com>
2024-11-21 08:05:16 -0500

    springboot app - initial

---
+++ src/test/java/io/kalaisaran/spring_github_OAuth_login_starter/SpringGithubOAuthLoginStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kalaisaran.spring_github_OAuth_login_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubOAuthLoginStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
